55a54d6c91c41c765d8a7e7bf4c51f9ea5fa3b59
Samuel Akinniranye <sakinnir@purdue.edu>
2025-12-19 16:32:05 -0500

    Initial Commit

---
+++ RealTimeChat/RealTimeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeChatApp.swift
+//  RealTimeChat
+//
+//  Created by Samuel Akinniranye on 12/19/25.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
