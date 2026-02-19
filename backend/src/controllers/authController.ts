39c60ab05027ccec653bca92a79d90e0a02be7d8
Samuel Akinniranye <sakinnir@purdue.edu>
2026-02-19 08:31:11 -0500

    register implementation

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,9 +1,46 @@
 import express, { Request, Response } from "express";
+import { prisma } from "../config/prisma";
+import { validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
 
-export const registerUser = (req: Request, res: Response) => {
-  res.send({
-    message: `creating a new user`,
-  });
+
+// validationResult, checks to see if any validation fails
+export const registerUser = async(req: Request, res: Response) => {
+  const errors = validationResult(req)
+
+  if (errors.isEmpty() === false) {
+    return res.status(400).json({
+      status: 'error',
+      msg: 'Validation error',
+      errors: errors.array()
+    })
+  }
+
+  try {
+    const { email, password } = req.body;
+    const existingUser = await prisma.user.findUnique({where: { email }});
+    if (existingUser) return res.status(409).json({ error: "Email is already in use" });
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword}
+    })
+    return res.status(201).json({
+      status: 'success',
+      msg: 'User created successfully.',
+      user: {
+        id: user.id,
+        email: user.email
+      }
+    });
+  }catch (error: any) {
+    console.error(error);
+    return res.status(500).json({
+      status: 'error',
+      msg: 'Internal server error.',
+      errors: error.message
+    })
+  }
 };
 
 export const loginUser = (req: Request, res: Response) => {
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -4,7 +4,12 @@ const { body } = require('express-validator');
 
 const router = Router();
 
-router.post("/register", registerUser);
+const validateRegister = [
+    body('email').isEmail().notEmpty().withMessage('Please enter your email address.'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be atleast 8 characters long.')
+]
+
+router.post("/register", validateRegister, registerUser);
 router.post("/login", loginUser);
 
 export default router;
